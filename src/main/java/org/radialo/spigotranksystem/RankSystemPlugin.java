56df4f9d7827564a1e43b01824336091e10bb5ad
RadiALo <78644882+RadiALo@users.noreply.github.com>
2024-02-24 15:22:22 +0200

    Initial commit

---
+++ src/main/java/org/radialo/spigotranksystem/RankSystemPlugin.java
@@ -0,0 +1,11 @@
+package org.radialo.spigotranksystem;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RankSystemPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+}
